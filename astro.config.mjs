ae41cacb5828c1ffc089feb47513c87c9dc27cc1
o7digital <101292703+o7digital@users.noreply.github.com>
2026-02-19 20:42:21 -0600

    Clean WP legacy assets and fix mobile menu language switch

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import react from '@astrojs/react';
-
 // https://astro.build/config
 export default defineConfig({
-  site: process.env.SITE_URL || process.env.PUBLIC_SITE_URL || 'https://jeanlouisdavid.com.mx',
-  integrations: [react()]
+  site: process.env.SITE_URL || process.env.PUBLIC_SITE_URL || 'https://jeanlouisdavid.com.mx'
 });
